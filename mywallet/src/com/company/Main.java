cde210de629c200756e1e3fb5c8e45aea799e56b
dinkli <108268032+dinkli@users.noreply.github.com>
2022-07-02 21:21:09 +0300

    Update Main.java

--- mywallet/src/com/company/Main.java
+++ mywallet/src/com/company/Main.java
@@ -2,6 +2,12 @@ package com.company;
 
 import java.util.Scanner;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Мой кошелек");
@@ -11,15 +17,16 @@ public class Main {
         System.out.println("4 Показать все покупки");
         System.out.println("5 Выход из программы");
         System.out.println(" ");
-        System.out.println("Введите одну из цифр от 1 до 5 и нажмите на Enter");
+        System.out.print("Введите одну из цифр от 1 до 5 и нажмите на Enter ");
         Scanner console = new Scanner(System.in);
         int number = console.nextInt();
         switch (number) {
             case 1:
                 System.out.print("Введите сумму для внесения денег в кошелек: ");
-                //Scanner addMoney = new Scanner(System.in);
-                //int money = addMoney.nextInt();
-
+                Scanner addMoney = new Scanner(System.in);
+                int money = addMoney.nextInt();
+                DatabaseHandler dbHandler = new DatabaseHandler(); //объект класса DatabaseHandler
+                dbHandler.enterDataIn(addMoney);
             case 2:
                 break;
             case 3:
