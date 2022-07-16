2fb931eeb7f204f46e3c5b0ae6de445362fc28d6
dinkli <108268032+dinkli@users.noreply.github.com>
2022-07-16 10:09:56 +0300

    Update Main.java

--- mywallet/src/com/company/Main.java
+++ mywallet/src/com/company/Main.java
@@ -2,14 +2,12 @@ package com.company;
 
 import java.util.Scanner;
 
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
 public class Main {
     public static void main(String[] args) {
+        boolean flag = true;
+        Scanner sc = new Scanner(System.in);
+        DatabaseHandler dbHandler = new DatabaseHandler(); // объект класса DatabaseHandler
+        do {
         System.out.println("Мой кошелек");
         System.out.println("1 Внести деньги в кошелек");
         System.out.println("2 Внести покупку");
@@ -18,27 +16,66 @@ public class Main {
         System.out.println("5 Выход из программы");
         System.out.println(" ");
         System.out.print("Введите одну из цифр от 1 до 5 и нажмите на Enter ");
-        Scanner console = new Scanner(System.in);
-        int number = console.nextInt();
-        switch (number) {
-            case 1:
-                System.out.print("Введите сумму для внесения денег в кошелек: ");
-                Scanner addMoney = new Scanner(System.in);
-                int money = addMoney.nextInt();
-                DatabaseHandler dbHandler = new DatabaseHandler(); //объект класса DatabaseHandler
-                dbHandler.enterDataIn(addMoney);
-            case 2:
-                break;
-            case 3:
-            break;
-            case 4:
-                break;
-            case 5:
-                //System.exit(0);
-                break;
-            default:
-                System.out.println("Вы ввели не существующий номер из меню");
-                break;
-        }
+        int number = 0;
+            try {
+                number = sc.nextInt();
+                if (number <= 0 || number > 5)
+                    throw new Exception();
+            } catch (Exception e) {
+                System.err.println("Ошибка ввода! Пункт меню должен числом быть от 1 до 5!\n");
+                sc.nextLine();
+                continue;
+            }
+            switch (number) {
+                case 1: {
+                    System.out.print("Введите сумму для внесения денег в кошелек: ");
+                    int addMoney = 0;
+                    try {
+                        addMoney = sc.nextInt();
+                        if (addMoney <= 0)
+                            throw new Exception();
+                        dbHandler.IncreaseBalance(addMoney);
+                        System.out.println("Баланс успешно пополнен!\n");
+                    } catch (Exception e) {
+                        System.err.println("Ошибка ввода! Сумма должна быть только целым положительным числом!\n");
+                        sc.nextLine();
+                        continue;
+                    }
+                    break;
+                }
+                case 2: {
+                    sc.nextLine();
+                    System.out.print("Введите название покупки: ");
+                    String name = sc.nextLine();
+                    System.out.print("Введите стоимость покупки: ");
+                    int price = 0;
+                    try {
+                        price = sc.nextInt();
+                        if (price <= 0)
+                            throw new Exception();
+                        dbHandler.AddPurchase(name, price);
+                        System.out.println("Покупка успешно внесена!\n");
+                    } catch (Exception e) {
+                        System.err.println("Ошибка ввода! Стоимость должна быть только целым положительным числом!\n");
+                        sc.nextLine();
+                        continue;
+                    }
+                    break;
+                }
+                case 3:
+                    System.out.println("\nТекущий баланс = " + dbHandler.GetBalance());
+                    break;
+                case 4:{
+                    System.out.println("Все покупки:");
+                    dbHandler.PrinttAllPurchases();
+                    System.out.println();
+                    break;
+                }
+                case 5:
+                    flag = false;
+                    break;
+            }
+        } while (flag);
+        sc.close();
     }
 }
